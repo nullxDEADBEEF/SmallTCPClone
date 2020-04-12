fabfea005cde442c0d936a3ce4ec6fc411b932e7
nullxDEADBEEF <andxdgu@gmail.com>
2020-04-12 21:47:10 +0200

    Put logic into functions

--- src/client.py
+++ src/client.py
@@ -7,23 +7,45 @@ server_address = ( 'localhost', 10000 )
 
 QUIT_MESSAGE = b"QUIT"
 
-connected = True
-while connected:
-    try:
-        # encode the message into bytes
-        message = input( "> " ).encode()
-        # send data
-        print( 'sending {!r}'.format( message ) )
-        sent = sock.sendto( message, server_address )
-        if message == QUIT_MESSAGE:
-            connected = False
-        else:
-            # receive response
-            print( 'waiting to receive' )
-            data, server = sock.recvfrom( 4096 )
-            print( 'received {!r}'.format( data ) )
-    except OSError:
-        print( "Error with the socket" )
+com_counter = 0
+msg_counter = 0
 
-print( 'closing socket')
-sock.close()
+def handle_handshake():
+    print( b"com-0 " )
+    ip = input().encode()
+    sock.sendto( ip, server_address )
+
+
+def send_message():
+    # encode message into bytes
+    message = input( "> " ).encode()
+    # send data
+    print( "sending {!r}".format( message ) )
+    sock.sendto( message, server_address )
+    return message
+
+
+def receive_response():
+    print( "waiting to receive" )
+    data, server = sock.recvfrom( 4096 )
+    print( "received {!r}".format( data ) )
+
+
+def handle_messages():
+    connected = True
+    while connected:
+        try:
+            send = send_message()
+            if send == QUIT_MESSAGE:
+                connected = False
+            else:
+                receive_response()
+        except OSError as ex:
+            print( "Error: {}", ex )
+
+    print( 'closing socket' )
+    sock.close()
+
+
+handle_handshake()
+handle_messages()
--- src/server.py
+++ src/server.py
@@ -4,25 +4,29 @@ import socket
 sock = socket.socket( socket.AF_INET, socket.SOCK_DGRAM )
 
 # bind socket to port
-server_address = ('localhost', 10000)
+server_address = ( 'localhost', 10000 )
 print( 'starting ip on {} port {}'.format( *server_address ) )
 sock.bind( server_address )
 
 QUIT_MESSAGE = b"QUIT"
 
-connected = True
-while connected:
-    print( '\nwaiting to receive message' )
-    data, address = sock.recvfrom( 4096 )
 
-    print( 'received {} bytes from {}'.format( len( data ), address ) )
+def handle_client_message():
+    running = True
+    while running:
+        print( '\nwaiting to receive message' )
+        data, address = sock.recvfrom( 4096 )
 
-    if data:
-        if data == QUIT_MESSAGE:
-            connected = False
-        else:
-            automated_message = b'I am server'
-            sent = sock.sendto( automated_message, address )
-            print( 'sent {} bytes back to {}'.format( sent, address ) )
+        print( 'received {} bytes from {}'.format( len( data ), address ) )
 
-sock.close()
+        if data:
+            if data == QUIT_MESSAGE:
+                running = False
+            else:
+                automated_message = b'I am server'
+                sent = sock.sendto( automated_message, address )
+                print( 'sent {} bytes back to {}'.format( sent, address ) )
+    sock.close()
+
+
+handle_client_message()
