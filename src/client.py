34ac7fffdb7bfb82675160287399b30a3efa54cf
nullxDEADBEEF <andxdgu@gmail.com>
2020-04-18 21:49:27 +0200

    Added client shutdown if connection is refused

--- src/client.py
+++ src/client.py
@@ -1,34 +1,48 @@
 import socket
+import sys
+
+# TODO: update msg-counter
+
 
 # create UDP socket
 sock = socket.socket( socket.AF_INET, socket.SOCK_DGRAM )
 
-server_address = ( 'localhost', 10000 )
+server_address = ( "127.0.0.1", 10000 )
 
 QUIT_MESSAGE = b"QUIT"
 
-com_counter = 0
 msg_counter = 0
+ip = socket.gethostbyname( socket.gethostname() )
 
-def handle_handshake():
-    print( b"com-0 " )
-    ip = input().encode()
-    sock.sendto( ip, server_address )
+
+def do_handshake():
+    com_counter = 0
+    print( "> com-{} ".format( com_counter ) + ip )
+
+    sock.sendto( ip.encode(), server_address )
+    server_acceptance, server = sock.recvfrom( 4096 )
+    print( server_acceptance.decode() )
+    client_accept = input().encode()
+    sock.sendto( client_accept, server_address )
+    if client_accept != "accept".encode():
+        print( "You didnt accept the connection" )
+        print( "Shutting down...." )
+        sys.exit( 0 )
+    com_counter += 1
 
 
 def send_message():
     # encode message into bytes
     message = input( "> " ).encode()
     # send data
-    print( "sending {!r}".format( message ) )
+    print( "msg-{} = {}".format( msg_counter, message.decode() ) )
     sock.sendto( message, server_address )
     return message
 
 
 def receive_response():
-    print( "waiting to receive" )
     data, server = sock.recvfrom( 4096 )
-    print( "received {!r}".format( data ) )
+    print( "res-{} = {}".format( msg_counter, data.decode() ) )
 
 
 def handle_messages():
@@ -41,11 +55,11 @@ def handle_messages():
             else:
                 receive_response()
         except OSError as ex:
-            print( "Error: {}", ex )
+            print( "Error: {}".format( ex ) )
 
-    print( 'closing socket' )
+    print( "closing socket" )
     sock.close()
 
 
-handle_handshake()
+do_handshake()
 handle_messages()
--- src/server.py
+++ src/server.py
@@ -4,20 +4,33 @@ import socket
 sock = socket.socket( socket.AF_INET, socket.SOCK_DGRAM )
 
 # bind socket to port
-server_address = ( 'localhost', 10000 )
-print( 'starting ip on {} port {}'.format( *server_address ) )
+server_address = ( "127.0.0.1", 10000 )
+print( "starting ip on {} port {}".format( *server_address ) )
 sock.bind( server_address )
 
 QUIT_MESSAGE = b"QUIT"
+client_connected = False
+
+
+def handle_handshake():
+    client_ip, address = sock.recvfrom( 4096 )
+    # TODO  : replace with com counter
+    sock.sendto( ( "> com-0 accept " + server_address[0] ).encode(), address )
+    data, address = sock.recvfrom( 4096 )
+    if data.decode() == "accept":
+        print( "Client accepted connection request" )
+    else:
+        print( "Client didnt accept connection request" )
+        handle_handshake()
 
 
 def handle_client_message():
     running = True
     while running:
-        print( '\nwaiting to receive message' )
+        print( "\nwaiting to receive message" )
         data, address = sock.recvfrom( 4096 )
 
-        print( 'received {} bytes from {}'.format( len( data ), address ) )
+        print( "received {} bytes from {}".format( len( data ), address ) )
 
         if data:
             if data == QUIT_MESSAGE:
@@ -25,8 +38,10 @@ def handle_client_message():
             else:
                 automated_message = b'I am server'
                 sent = sock.sendto( automated_message, address )
-                print( 'sent {} bytes back to {}'.format( sent, address ) )
+                print( "sent {} bytes back to {}".format( sent, address ) )
+    print( "Closing socket..." )
     sock.close()
 
 
+handle_handshake()
 handle_client_message()
